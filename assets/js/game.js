0e251e48569a7985c67df075ac8d7721810856cc
Samantha-Ruth <sam.haberman@gmail.com>
2022-04-27 17:13:51 -0600

    first few lines of code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a messate");
+//this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight();
+
